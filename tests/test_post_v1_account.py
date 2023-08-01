4cdcacf04f2a953b2707285cc4fd08284b441198
Anya150493 <Anya150493@mail.ru>
2023-08-01 19:29:19 +0300

    Работа с ORM

--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,6 +1,7 @@
 import time
 from generic.helpers.dm_db import DmDatabase
 from services.dm_api_account import Facade
+from generic.helpers.orm_db import OrmDatabase
 import structlog
 
 structlog.configure(
@@ -13,13 +14,21 @@ structlog.configure(
 def test_post_v1_account():
     api = Facade(host='http://localhost:5051')
 
-    login = "login_70"
-    email = "login_70@mail.ru"
-    password = "login_70"
+    login = "login_69"
+    email = "login_69@mail.ru"
+    password_user = "login_69"
 
-    db = DmDatabase(user='postgres', password='admin', host='localhost', database='dm3.5')
-    db.delete_user_by_login(login=login)
-    dataset = db.get_user_by_login(login=login)
+    user = 'postgres'
+    password = 'admin'
+    host = 'localhost'
+    database = 'dm3.5'
+
+    orm = OrmDatabase(user=user, password=password, host=host, database=database)
+    orm.delete_user_by_login(login="login_69")
+
+    # db = DmDatabase(user='postgres', password='admin', host='localhost', database='dm3.5')
+    # db.delete_user_by_login(login=login)
+    dataset = orm.get_user_by_login(login="login_69")
     assert len(dataset) == 0
 
     api.mailhog.delete_all_messages()
@@ -27,21 +36,23 @@ def test_post_v1_account():
     response = api.account.register_new_user(
         login=login,
         email=email,
-        password=password
+        password=password_user
     )
 
-    dataset = db.get_user_by_login(login=login)
+    dataset = orm.get_user_by_login(login="login_69")
     for row in dataset:
-        assert row['Login'] == login, f'User {login} not registered'
-        assert row['Activated'] is False, f'User {login} was activated'
+        assert row.Login == login, f'User {login} not registered'
+        assert row.Activated is False, f'User {login} was activated'
 
     api.account.activate_registered_user(login=login)
 
     time.sleep(2)
-    dataset = db.get_user_by_login(login=login)
+    dataset = orm.get_user_by_login(login="login_69")
     for row in dataset:
-        assert row['Activated'] is True, f'User {login} not activated'
+        assert row.Activated is True, f'User {login} not activated'
     api.login.login_user(
         login=login,
-        password=password
+        password=password_user
     )
+
+    orm.db.close_connection()
